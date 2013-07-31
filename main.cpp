880d5e4a9539244bf733314b3481bfe23a05d550
Dmitry Dementev <dem@oldschooldev.com>
2013-07-31 12:45:32 +0400

    sample

---
+++ main.cpp
@@ -0,0 +1,11 @@
+/* #include <emscripten/val.h> */
+
+
+int main(int argc, char **argv) {
+  /* using namespace emscripten; */
+  /* val doc = val::global("document"); */
+  /* val page = doc.call<val>("createElement", val("div")); */
+
+  /* doc["body"].call<val>("appendChild", page); */
+  return 0;
+}
